3466e0d909db2452c97e197cab4c1c2c26b0f939
bciccarelli <bciccarelli6@gmail.com>
2026-01-28 14:42:43 -0700

    wording and gradient

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function HomePage() {
                 You have a life.
               </h1>
               <h1 className="text-3xl font-bold tracking-tight text-primary mb-2">
-                Get a protocol that works around it. 
+                Get a protocol that works for it. 
               </h1>
               <h2 className="text-1xl font-bold tracking-tight text-primary mb-4">
                 (For free)
